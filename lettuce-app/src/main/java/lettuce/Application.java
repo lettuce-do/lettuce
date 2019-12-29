b8eebfc0c596094042ce501c6657b6e81bce81be
brandon.hwang <brandon.hwang@workday.com>
2019-12-28 18:44:23 -0800

    inherit from starter spring boot

--- lettuce-app/src/main/java/lettuce/Application.java
+++ lettuce-app/src/main/java/lettuce/Application.java
@@ -1,8 +1,8 @@
 package lettuce;
 
 public class Application {
-  public static void main(final String[] args) {
-    final Response resp = new Response();
-    System.out.println(resp.ping());
-  }
+	public static void main(final String[] args) {
+		final Response resp = new Response();
+		System.out.println(resp.ping());
+	}
 }
--- lettuce-app/src/main/java/lettuce/Response.java
+++ lettuce-app/src/main/java/lettuce/Response.java
@@ -3,8 +3,8 @@ package lettuce;
 import java.time.Instant;
 
 public class Response {
-  public String ping() {
-    Instant timestamp = Instant.now();
-    return  timestamp + "lettuce - ping";
-  }
+	public String ping() {
+		Instant timestamp = Instant.now();
+		return timestamp + "lettuce - ping";
+	}
 }
